13c1a29b18b81c21e743121382a12c4799802f56
MatthewsTomts <learningahh@gmail.com>
2021-08-02 22:10:29 -0300

    aula16exB

--- Aula16ex/ex018/script.js
+++ Aula16ex/ex018/script.js
@@ -1,56 +1,62 @@
 var lista = []
-function analisar() {
-    var res = document.getElementById('res')
-    var txtnum = Number(document.getElementById('txtnum').value)
-    var tab = document.getElementById('seltab')
-    var tem = false
+var res = document.getElementById('res')
+var tab = document.getElementById('seltab')
 
-    res.innerHTML = ''
-
-    for (let num in lista) {
-        if (lista[num] == txtnum) {
+function inLista(veri, verLista) {
+    /*
+    for (let num in verLista) {
+        if (lista[num] == veri) {
             tem = true
+        } else {
+            tem = false
         }
     }
+    */
 
-    if (txtnum > 0 && txtnum < 101 && !tem) {
+    if (verLista.indexOf(veri) != -1){
+        return true
+    } else
+    // As chaves ({}) só são necessárias quando é executado mais de um comando na condição
+        return false
+}
+
+function analisar() {
+    var txtnum = Number(document.querySelector('#txtnum').value)
+    res.innerHTML = ''
+
+    if (txtnum > 0 && txtnum < 101 && !inLista(txtnum, lista)) {
         lista.push(txtnum)
         tab.innerHTML += `<option value="${txtnum}">Valor ${txtnum} adicionado</option>`
-    } else {
+    } else
         alert('Valor inválido ou já adicionado na lista.')
-    }
+
+    document.querySelector('#txtnum').value = ''
+    document.querySelector('#txtnum').focus()  // Faz com que a caixa de digitação seja selecionada
 }
 
 function finalizar() {
     var res = document.getElementById('res')
-    var maior = 0
-    var menor = 0
+    var maior = lista[0]
+    var menor = lista[0]
     var soma = 0
     var med = 0
 
     if (lista.length != 0) {
+        res.innerHTML = ''
         for (let num in lista) {
             soma += lista[num]
-            if (num == 0) {
+            if (lista[num] > maior)  
                 maior = lista[num]
-                menor = lista[num]
-            } else {
-                if (lista[num] > maior) {
-                    maior = lista[num]
-                }
-                if (lista[num] < menor) {
-                    menor = lista[num]
-                }
-            }
-            med = soma / lista.length
+            if (lista[num] < menor)
+                menor = lista[num]  
         }
+        med = soma / lista.length
 
         res.innerHTML += `<p>Ao todo, temos ${lista.length} números cadastrados</p>`
         res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
         res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
-        res.innerHTML += `<p>Somando todos os valores, temos ${soma}</p>`
+        res.innerHTML += `<p>Somando todos os vaores, temos ${soma}</p>`
         res.innerHTML += `<p>A média dos valores digitados é ${med}</p>`
-    } else {
+    } else
         alert('Adicione valores antes de finalizar.')
-    }
 }
